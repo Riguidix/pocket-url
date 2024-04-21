6acc7ed0522156e93b2c4d5be59ef834c8469390
Riguidix Rodriguez <reydi1@hotmail.com>
2024-04-21 17:32:01 -0600

    Removing default endpoints

--- server/app.js
+++ server/app.js
@@ -14,4 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', require('./routes/'));
+
 module.exports = app;
