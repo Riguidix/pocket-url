391d1613eb992a7fbf3210a800a121cf992a8205
Riguidix Rodriguez <reydi1@hotmail.com>
2024-04-23 13:14:54 -0600

    Adding app controll access

--- server/app.js
+++ server/app.js
@@ -19,11 +19,12 @@ app.use(
     origin: "*",
   })
 );
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use("/api", require("./routes/"));
 
