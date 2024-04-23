4b7d7ca55cf8671190c2775ef17cba827d5f635b
Riguidix Rodriguez <reydi1@hotmail.com>
2024-04-23 17:34:47 -0600

    Adding media breakpoints to layout

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,16 +4,19 @@ import Table from "../components/layouts/Table";
 
 export default function HomePage() {
   return (
-    <div className="bg-zinc-200 dark:bg-green-900 grid grid-rows-12 h-screen text-green-900 dark:text-white w-screen">
-      <div className="row-span-1 w-full">
+    <div className="flex flex-col h-screen items-center w-screen">
+      {/* <div className="row-span-1 w-full"></div> */}
+      <div className="w-full">
         <Header />
       </div>
 
-      <div className="row-span-6 w-full">
+      {/* <div className="row-span-6 w-full"> */}
+      <div className="h-2/3 w-3/4 md:w-2/3 lg:w-full">
         <Main />
       </div>
 
-      <div className="row-span-5 flex items-center justify-center w-full">
+      {/* <div className="row-span-5 flex items-center justify-center w-full"> */}
+      <div className="w-full md:w-1/2 lg:w-2/3">
         <Table />
       </div>
     </div>
