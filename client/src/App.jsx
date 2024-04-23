8bc4a96b7586358f7214e3ca701f9334caf681ed
Riguidix Rodriguez <reydi1@hotmail.com>
2024-04-23 17:26:30 -0600

    Removing unused classes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import AppRouter from "./AppRouter";
 
 export default function App() {
   return (
-    <div className="bg-zinc-200 dark:bg-green-900 grid grid-rows-12 h-screen text-green-900 dark:text-white w-screen">
+    <div className="bg-zinc-200 dark:bg-green-900 h-screen text-green-900 dark:text-white w-screen">
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
