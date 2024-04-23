17f6b9549ae19d188a2b89dd93d594de85d9b7f8
Riguidix Rodriguez <reydi1@hotmail.com>
2024-04-23 12:59:49 -0600

    Adding middleware control origin

--- server/app.js
+++ server/app.js
@@ -1,27 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-
-let corsOptions = {
-    origin: 'http://example.com'
-}
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
 var app = express();
-require('./config/database').mongoConnect();
+require("./config/database").mongoConnect();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-    origin: "*"
-}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
-app.use('/api', require('./routes/'));
+app.use("/api", require("./routes/"));
 
 module.exports = app;
